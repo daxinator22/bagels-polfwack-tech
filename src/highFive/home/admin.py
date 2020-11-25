ef97d4d10299c46f5ab663f9fac518a18d7a82d2
Adam Thomas <adam.thomas@aggiemail.usu.edu>
2020-11-25 12:21:20 -0600

    Fixed Ingredients model

--- src/highFive/home/admin.py
+++ src/highFive/home/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import foodItem
+from .models import foodItem, Ingredients
 
 admin.site.register(foodItem)
+admin.site.register(Ingredients)
 
 # Register your models here.
--- src/highFive/home/models.py
+++ src/highFive/home/models.py
@@ -26,6 +26,7 @@ class Customer(models.Model):
 class Ingredients(models.Model):
     type = models.CharField(max_length=200)
     price = models.DecimalField(decimal_places=2, max_digits=10)
+    inv_count = models.IntegerField()
     def __str__(self):
         return self.type
 
