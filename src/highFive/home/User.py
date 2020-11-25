010a40aaf5881c3b46589a3ff9142649b1696cbb
Adam Thomas <adam.thomas@aggiemail.usu.edu>
2020-11-24 21:39:57 -0600

    Added Ingredient and sandwich models

--- src/highFive/home/User.py
+++ src/highFive/home/User.py
@@ -1,10 +1,12 @@
 class User:
-    
+
     def __init__(self, name, username, password, emailAddress):
         self.name = name
         self.username = username
         self.password = password
         self.emailAddress = emailAddress
+        self.funds = 0.00
+
 
     def printName(self):
         print(f'My name is {self.name}, my username is {self.username} and my password is {self.password}')
--- src/highFive/home/models.py
+++ src/highFive/home/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+
 import datetime
 from django.utils import timezone
 
@@ -16,5 +17,23 @@ class Customer(models.Model):
     username = models.CharField(max_length=200)
     password = models.CharField(max_length=200)
     emailAddress = models.CharField(max_length=200)
+    funds = models.DecimalField(max_digits=10, decimal_places=2)
+
     def __str__(self):
         return self.name
+
+
+class Ingredients(models.Model):
+    type = models.CharField(max_length=200)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    def __str__(self):
+        return self.type
+
+
+class BagelSandwich(models.Model):
+    ingredients = []
+
+
+class Order(models.Model):
+    sandwiches = []
+    bagels = []
