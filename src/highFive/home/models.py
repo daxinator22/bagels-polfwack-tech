529c43e7c69944ed31972a46f2a1c3823102a0ce
Adam Thomas <adam.thomas@aggiemail.usu.edu>
2020-11-30 20:06:15 -0700

    Added Bagel Sandwiches

--- src/highFive/home/models.py
+++ src/highFive/home/models.py
@@ -4,19 +4,23 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 import datetime
 from django.utils import timezone
+import random
 
 # Create your models here.
 class foodItem(models.Model):
     type = models.CharField(max_length=200)
     sub_type = models.CharField(max_length=200)
-    inv_count = models.IntegerField()
+    inv_count = models.IntegerField(default=random.randint(1000, 140000))
     price = models.DecimalField(decimal_places=2,max_digits=10)
+    #item_id = models.IntegerField()
     def __str__(self):
         return self.sub_type
     def addToInv(self, amount):
         self.inv_count += amount
     def removeFromInv(self, amount):
         self.inv_count -= amount
+
+
 
 
 
@@ -34,6 +38,8 @@ class Ingredients(models.Model):
 
 class BagelSandwich(models.Model):
     ingredients = []
+    def addItem(self, item):
+        self.ingredients.append(item)
 
 
 class Order(models.Model):
@@ -41,6 +47,7 @@ class Order(models.Model):
     isMade = models.BooleanField(default=False)
     isFilled = models.BooleanField(default=False)
     bagels = []
+    sandwiches = []
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
